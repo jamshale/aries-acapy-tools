73ca43b56837b9d0ed1fa0f90052e33a52927e68
jamshale <jamiehalebc@gmail.com>
2026-07-06 20:22:26 +0000

    Delete issuer_rev_reg records correctly
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- askar_tools/anoncreds_repair.py
+++ askar_tools/anoncreds_repair.py
@@ -61,8 +61,10 @@ class Repairer:
             await store.close()
 
     async def _repair_revocation_registries(self, txn):
-        for record in await txn.fetch_all("revocation_reg_def"):
-            if await txn.fetch("revocation_reg_def_private", name=record.name):
+        for record in await txn.fetch_all("issuer_rev_reg"):
+            if await txn.fetch(
+                "revocation_reg_def_private", name=record.tags.get("revoc_reg_id")
+            ):
                 continue
             print(f"Removing broken revocation registry {record.name}")
             try:
